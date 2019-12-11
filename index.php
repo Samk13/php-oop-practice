4183c2d65e6e63207602542bbc495f79089b1774
sam_arbid <samarbid13@users.noreply.github.com>
2019-12-11 14:29:25 +0100

    validate users

--- index.php
+++ index.php
@@ -1,39 +1,44 @@
 <?php 
 
-echo <<<EOH
+  require('user_validator.php');
 
- ______     ______     __    __        ______     ______     ______     __     _____    
-/\  ___\   /\  __ \   /\ "-./  \      /\  __ \   /\  == \   /\  == \   /\ \   /\  __-.  
-\ \___  \  \ \  __ \  \ \ \-./\ \     \ \  __ \  \ \  __<   \ \  __<   \ \ \  \ \ \/\ \ 
- \/\_____\  \ \_\ \_\  \ \_\ \ \_\     \ \_\ \_\  \ \_\ \_\  \ \_____\  \ \_\  \ \____- 
-  \/_____/   \/_/\/_/   \/_/  \/_/      \/_/\/_/   \/_/ /_/   \/_____/   \/_/   \/____/ 
-     
-  
-EOH;
+  $errors = [];
 
-class Weather {
-    public static $tempConditions = ['cold','mild','warm'];
+  if(isset($_POST['submit'])){
+    // validate entries
+    $validation = new UserValidator($_POST);
+    $errors = $validation->validateForm();
 
-    public static function celsiusToFareneit($c){
-        $cf= $c * 9 / 5 +32;
-        return "$c C converted to F ===> $cf F";
-    }
-    public static function determineTempCondition($f){
-        if ($f < 40 ){
-            return " \n\t ====> today the wheather is " . self :: $tempConditions[0] . "❌";
-
-        } elseif($f < 70){
-            return self :: $tempConditions[1];
-        }else{
-            return self :: $tempConditions[2];
-        }
-    }
-}
-
-print_r( Weather :: $tempConditions);
-
-echo Weather :: celsiusToFareneit(20);
-
-echo Weather :: determineTempCondition(10);
+    // if errors is empty --> save data to db
+  }
 
 ?>
+
+<html lang="en">
+<head>
+  <title>PHP OOP</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  
+  <div class="new-user">
+    <h2>Create a new user</h2>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+
+      <label>username: </label>
+      <input type="text" name="username" value="<?php echo htmlspecialchars($_POST['username']) ?? '' ?>">
+      <div class="error">
+        <?php echo $errors['username'] ?? '' ?>
+      </div>
+      <label>email: </label>
+      <input type="text" name="email" value="<?php echo htmlspecialchars($_POST['email']) ?? '' ?>">
+      <div class="error">
+        <?php echo $errors['email'] ?? '' ?>
+      </div>
+      <input type="submit" value="submit" name="submit" >
+
+    </form>
+  </div>
+
+</body>
+</html>
